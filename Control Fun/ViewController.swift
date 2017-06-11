19ae2b8c1db193a1f98c38966dd04a15c030b754
laileon <leon@igola.com>
2017-06-11 17:05:59 +0800

    add text editor

--- Control Fun/ViewController.swift
+++ Control Fun/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBAction func onSliderChanged(_ sender: UISlider) {
+        sliderLabel.text = "\(lroundf(sender.value))"
+    }
 
+    @IBOutlet weak var sliderLabel: UILabel!
     @IBOutlet weak var numberField: UITextField!
     @IBOutlet weak var nameField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        sliderLabel.text = "50"
     }
 
     override func didReceiveMemoryWarning() {
